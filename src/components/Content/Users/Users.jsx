ff29a86948e3c068b330f8e98de32c65d09d9dee
fenK <mixail.syursin@gmail.com>
2020-06-18 16:34:40 +0600

    51

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -1,91 +1,48 @@
-import React from 'react';
-import s from './Users.module.css'
+import React      from 'react';
+import s          from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from "../../../assets/user.png";
 
 const Users = (props) => {
-    if (!props.users.length){
-        props.setUsers([
-            {
-                id: 1,
-                fullName: 'Dima',
-                location: {
-                    city: 'Kiev',
-                    country: `Ukraine`,
-                },
-                status: `I'm a boss`,
-                isFollowed: true,
-                photoUrl:`https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg`
-            },
-            {
-                id: 2,
-                fullName: 'Sveta',
-                location: {
-                    city: 'Ust-Kamenogorsk',
-                    country: `KZ`,
-                },
-                status: `I'm a boss`,
-                isFollowed: false,
-                photoUrl:`https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg`
-            },
-            {
-                id: 3,
-                fullName: 'Sasha',
-                location: {
-                    city: 'Moscow',
-                    country: `Russia`,
-                },
-                status: `I'm a boss`,
-                isFollowed: true,
-                photoUrl:`https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg`
-            },
-            {
-                id: 4,
-                fullName: 'Victor',
-                location: {
-                    city: 'London',
-                    country: `UK`,
-                },
-                status: `I'm a boss`,
-                isFollowed: false,
-                photoUrl:`https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg`
-            },
-            {
-                id: 5,
-                fullName: 'Valera',
-                location: {
-                    city: 'Dallas',
-                    country: `USA`,
-                },
-                status: `I'm a boss`,
-                isFollowed: true,
-                photoUrl:`https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg`
-            },
-        ])
-    }
-    return (
-        <div className={s.users}>
-            {
-                props.users.map(user => (
-                        <div className={s.user}>
-                            <div className={s.user_left}>
-                                <img src={user.photoUrl} alt=""/>
-                            </div>
-                            <div className={s.user_right}>
-                                <div className={s.text}>
-                                    <span className={s.name}>{user.fullName}</span>
-                                    <span className={s.location}>{user.location.country}, {user.location.city}</span>
-                                    <div className={s.status}>{user.status}</div>
+  if (!props.users.length) {
+    axios
+        .get('https://127.0.0.1:8000/api/user')
+        .then(response => {
+          const users = response.data.data.users || [];
+          props.setUsers(users);
+        });
+
+  }
+  return (
+      <div className={s.users}>
+        {
+          props.users.map(user => (
+                              <div className={s.user}>
+                                <div className={s.user_left}>
+                                  <img src={user.photoUrl ? user.photoUrl : userPhoto} alt=""/>
                                 </div>
-                                { user.isFollowed ?
-                                    <button onClick={() =>props.onUnfollow(user.id)}>Отписаться</button> :
-                                    <button onClick={() =>props.onFollow(user.id)}>Подписаться</button>
-                                }
-                            </div>
-                        </div>
-                    ),
-                )
-            }
-        </div>
-    )
+                                <div className={s.user_right}>
+                                  <div className={s.text}>
+                                    <span className={s.name}>{user.fullName}</span>
+                                    <span
+                                        className={s.location}>{user.location.country}, {user.location.city}</span>
+                                    <div className={s.status}>{user.status}</div>
+                                  </div>
+                                  {user.isFollowed
+                                   ?
+                                   <button
+                                       onClick={() => props.onUnfollow(user.id)}>Отписаться</button>
+                                   :
+                                   <button
+                                       onClick={() => props.onFollow(user.id)}>Подписаться</button>
+                                  }
+                                </div>
+                              </div>
+                          ),
+          )
+        }
+      </div>
+  );
 };
 
 export default Users;
